20aeeb767b40db2e4a61549f0af1037e58fc90ef
mirx562 <mirx562@gmail.com>
2025-02-07 16:39:47 +0700

    fin

--- src/app/pdf/page.tsx
+++ src/app/pdf/page.tsx
@@ -1,12 +1,12 @@
 "use client";
-import { PDFViewer } from "@react-pdf/renderer";
-import React from "react";
-import MyPDFDocument from "./pdf-document";
+// import { PDFViewer } from "@react-pdf/renderer";
+// import MyPDFDocument from "./pdf-document";
 
 export default function page() {
   return (
-    <PDFViewer className="w-full h-screen">
-      <MyPDFDocument />
-    </PDFViewer>
+    <div>pdf</div>
+    // <PDFViewer className="w-full h-screen">
+    //   <MyPDFDocument />
+    // </PDFViewer>
   );
 }
