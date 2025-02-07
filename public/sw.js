81cb79006996d98a1fdc48b770b4fdcaee94d6e2
mirx562 <mirx562@gmail.com>
2025-02-07 16:45:08 +0700

    up

--- public/sw.js
+++ public/sw.js
@@ -18,5 +18,7 @@ self.addEventListener("push", function (event) {
 self.addEventListener("notificationclick", function (event) {
   console.log("Notification click received.");
   event.notification.close();
-  event.waitUntil(clients.openWindow("<https://your-website.com>"));
+  event.waitUntil(
+    clients.openWindow("https://testing-libraries-nextjs.vercel.app/standalone")
+  );
 });
